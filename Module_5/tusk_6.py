1b0405575029698c0579d9e533aab4ee6101426e
Руслан <ruslantaibov2003@mail.ru>
2022-11-01 01:00:41 +0300

    new

--- Module_5/tusk_6.py
+++ Module_5/tusk_6.py
@@ -1,11 +1,9 @@
 number = int(input())
 fibonachi_list = [1, 1]
-
 i = 0
 while True:
     i += 1
     fibonachi_list.append(fibonachi_list[i - 1] + fibonachi_list[i])
-
     if number == fibonachi_list[i]:
         print(i+1)
         break
