6e0671309c23a305d35b1f22dcae68ffc18c7023
Руслан <ruslantaibov2003@mail.ru>
2022-11-02 23:54:19 +0300

    new

--- Module_6/task_8.py
+++ Module_6/task_8.py
@@ -1,18 +1,12 @@
 stroka = input()
-
 print(stroka[2])
 print(stroka[len(stroka) - 1])
-
 for i in range(0, 5):
     print(stroka[i], end='')
-
 print('\n')
-
 for i in range(0, len(str) - 2):
     print(str[i], end='')
-
 print('\n')
-
 string_chet = ''
 string_nechet = ''
 for i in range(0, len(str)):
@@ -20,9 +14,7 @@ for i in range(0, len(str)):
         string_chet += str[i]
     else:
         string_nechet += str[i]
-
 print(string_chet)
 print(string_nechet)
-
 print(str[::-1])
 print(str[::-2])
