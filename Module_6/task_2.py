96397f2bad74e408f6b77c0b245014402afac736
Руслан <ruslantaibov2003@mail.ru>
2022-11-02 23:17:17 +0300

    new

---
+++ Module_6/task_2.py
@@ -0,0 +1,7 @@
+list = [i for i in input().split()]
+list_ans = []
+for a in range(0, len(list) - 1):
+    if int(list[a]) < int(list[a + 1]):
+        list_ans.append(list[a + 1])
+
+print(" ".join(list_ans))
