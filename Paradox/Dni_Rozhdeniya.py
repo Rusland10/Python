f47a54473a7148b55546878c66ed7734c5cdf07e
Руслан <ruslantaibov2003@mail.ru>
2022-11-03 17:02:03 +0300

    new

---
+++ Paradox/Dni_Rozhdeniya.py
@@ -0,0 +1,26 @@
+import random
+def birthday(people_counter, iteration):
+    days = [day for day in range(0, 29)]
+    months = [month for month in range(0, 13)]
+    count = 0
+    count_not = 0
+    for iterations in range(0, iteration+1):
+        l1 = []
+        l2 = []
+        for i in range(0, people_counter+1):
+            l1.append(random.choice(days))
+            l2.append(random.choice(months))
+        for i in range(0, people_counter+1):
+            if l1.count(l1[i]) == 1 or l2.count(l2[i]) == 1:
+                count_not += 1
+                pass
+            for j in range(0, people_counter+1):
+                if j == i:
+                    pass
+                if l1[j] == l1[i] and l2[j] == l2[i]:
+                    count += 1
+    return f"количество совпадений: {count} \n" \
+           f"количество промахов: {count_not} \n" \
+           f"вероятность совпадения: {(count * 100) / (count_not + count)}"
+if __name__ == "__main__":
+    print(birthday(23, 10000))
