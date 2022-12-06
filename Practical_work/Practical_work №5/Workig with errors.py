ad971ee1e3f8aa67ebee7844b791587ac9ca4d0c
Руслан <ruslantaibov2003@mail.ru>
2022-12-06 16:00:38 +0300

    new

--- Practical_work/Practical_work №5/Workig with errors.py
+++ Practical_work/Practical_work №5/Workig with errors.py
@@ -1,9 +1,40 @@
-def otk():
-    f = open('data')
+def otk(d):
+    f = open(d)
     a = f.readline()
     num_list = f.read().splitlines()
     num_list = [int(i) for i in num_list]
-
     return num_list
+try:
+    name = str(input('Введите имя файла: '))
+    print(otk(name))
+except FileNotFoundError:
+    print("Такого файла нет!")
+    rezultat = False
+except NameError:
+    print("Такого имени нет!")
+    rezultat = False
+except ValueError:
+    print("Значение в документе не соответсвует числу!")
+    rezultat = False
+except TypeError:
+    print("Несовместимые аргументы!")
+    rezultat  = False
+except ZeroDivisionError:
+    print('Неверные данные для итерации!!')
+    rezultat  = False
+except MemoryError:
+    print("Недостаточно оперативной памяти!")
+    rezultat  = False
+except StopIteration:
+    print("Неверные данные для итерации!*")
+    rezultat  = False
+except IndexError:
+    print("Неверные данные для итерации!/")
+    rezultat  = False
+except:
+    print("Fatality error!")
+    rezultat  = False
+else:
+    print(otk(name))
+    rezultat  = True
 
-print(otk())
