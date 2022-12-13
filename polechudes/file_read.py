39adb6639814ba96a5806d0cc6b327dc3c782478
Руслан <ruslantaibov2003@mail.ru>
2022-12-13 13:45:27 +0300

    new

--- polechudes/file_read.py
+++ polechudes/file_read.py
@@ -4,24 +4,18 @@ print(*text.title().split('\n'))
 '''
 
 import random
-
-
 def slova() -> str:
     '''
     :return: Строка - рандомное слово из списка, которое надо угадать.
     '''
     with open(r'polechudes/slova.txt', encoding='UTF8') as sl:
         return random.choice(sl.read().splitlines()).lower()
-
-
 def records() -> int:
     '''
     :return: максимальное количество очков за одну игровую сессию.
     '''
     with open(r'polechudes/records.txt', mode='r', encoding='UTF8') as r:
         return int(r.read())
-
-
 def writed(n: str):
     '''
     :param n: строка с числом максимальным количеством очков за одну игровую сессию.
