79fa91a52fbd31d1a6574d814f6527b15350f5b5
Руслан <ruslantaibov2003@mail.ru>
2022-12-16 20:26:34 +0300

    new

---
+++ Practical_work/Practical_work №4.py
@@ -0,0 +1,26 @@
+import pymorphy2
+import re
+from translate import Translator
+with open(r"C:\Users\nikita\Downloads\633740083-dialog.html", encoding='utf8') as f:
+    rez = f.read()
+pattern = r'(?:class=\"msg-body\">)([^<+]+)(?:</p>)'
+match = re.findall(pattern, rez)
+slov = []
+slovar = {}
+for i in range(len(match)):
+    slov.extend(match[i].split())
+morfi = pymorphy2.MorphAnalyzer()
+norm = [morfi.parse(i)[0].normal_form for i in slov]
+for i in norm:
+    if i in slovar:
+        continue
+    elif i not in slovar:
+        slovar[i] = norm.count(i)
+slovar = dict(sorted(slovar.items(), key=lambda item: item[1], reverse=True))
+print(slovar)
+trnsl = []
+translates = Translator(from_lang='ru', to_lang='English')
+# print(translates.translate())
+for i in slovar.keys():
+    trnsl.append(translates.translate(i))
+print(trnsl)
