a87d99332d610455cc60496a25face94c052d089
Руслан <ruslantaibov2003@mail.ru>
2022-12-19 17:55:08 +0300

    new

--- Practical_work/Dni_Rozhdeniya.py
+++ Practical_work/Dni_Rozhdeniya.py
@@ -1,26 +1,18 @@
 import random
-def birthday(people_counter, iteration):
-    days = [day for day in range(0, 29)]
-    months = [month for month in range(0, 13)]
+
+__all__ = ['birthday']
+def birthday(iteration, people_counter = 23 ):
     count = 0
-    count_not = 0
-    for iterations in range(0, iteration+1):
-        l1 = []
-        l2 = []
-        for i in range(0, people_counter+1):
-            l1.append(random.choice(days))
-            l2.append(random.choice(months))
-        for i in range(0, people_counter+1):
-            if l1.count(l1[i]) == 1 or l2.count(l2[i]) == 1:
-                count_not += 1
-                pass
-            for j in range(0, people_counter+1):
-                if j == i:
-                    pass
-                if l1[j] == l1[i] and l2[j] == l2[i]:
-                    count += 1
-    return f"количество совпадений: {count} \n" \
-           f"количество промахов: {count_not} \n" \
-           f"вероятность совпадения: {(count * 100) / (count_not + count)}"
+    for i in range(iteration):
+        age_list = []
+        for j in range(people_counter):
+            age = random.randint(1, 365)
+            if age in age_list:
+                count = count + 1
+                break
+            age_list.append(age)
+    return "Вероятность совпадения ДР: " + str(count/(iteration/100)) + " %"
+
+
 if __name__ == "__main__":
-    print(birthday(23, 10000))
+    print(birthday(15000, 23))
