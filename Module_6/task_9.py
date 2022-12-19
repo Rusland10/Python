0c201557f3c3b709c3b228cedfcc626c6c7e6688
Руслан <ruslantaibov2003@mail.ru>
2022-12-19 18:21:56 +0300

    new

--- Module_6/task_9.py
+++ Module_6/task_9.py
@@ -1,4 +1,6 @@
-a = input()
-left_a = a[len(a) // 2 + len(a) % 2:]
-right_a = a[: len(a) // 2 + len(a) % 2]
-print(left_a + right_a)
+word = input()
+if len(word) % 2 != 0:
+    left_word = word[:len(word) // 2]
+    right_word = word[len(word) // 2:]
+    left_word += word[(len(word) - 1) // 2]
+print(right_word + left_word)
