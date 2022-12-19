c5a53062cd919a276559be6d34206b07e9735e9c
Руслан <ruslantaibov2003@mail.ru>
2022-12-19 18:28:26 +0300

    new

--- Module_6/task_12.py
+++
@@ -1,5 +0,0 @@
-word = input()
-word_formatted = word[:word.find('h')]
-word_formatted += word[word.find('h')+1:word.rfind('h')+1][::-1]
-word_formatted += word[word.rfind('h'):]
-print(word_formatted)
