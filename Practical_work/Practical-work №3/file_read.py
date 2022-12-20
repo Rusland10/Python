7259fae20a150ddce645e4b975be5573f27d41b7
Руслан <ruslantaibov2003@mail.ru>
2022-12-20 13:45:28 +0300

    new

--- Practical_work/Practical-work №3/file_read.py
+++ Practical_work/Practical-work №3/file_read.py
@@ -8,18 +8,18 @@ def slova() -> str:
     '''
     :return: Строка - рандомное слово из списка, которое надо угадать.
     '''
-    with open(r'polechudes/slova.txt.txt', encoding='UTF8') as sl:
+    with open(r'Practical-work №3/slova.txt.txt', encoding='UTF8') as sl:
         return random.choice(sl.read().splitlines()).lower()
 def records() -> int:
     '''
     :return: максимальное количество очков за одну игровую сессию.
     '''
-    with open(r'polechudes/records.txt', mode='r', encoding='UTF8') as r:
+    with open(r'Practical-work №3/records.txt', mode='r', encoding='UTF8') as r:
         return int(r.read())
 def writed(n: str):
     '''
     :param n: строка с числом максимальным количеством очков за одну игровую сессию.
     :return: -
     '''
-    with open(r'polechudes/records.txt', mode='w', encoding='UTF8') as r:
+    with open(r'Practical-work №3/records.txt', mode='w', encoding='UTF8') as r:
         r.write(n)
